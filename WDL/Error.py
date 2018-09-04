9824b5bc7e5a3b0ee4ea3edbe06cb12415494dd5
Mike Lin <mlin@mlin.net>
2018-09-03 20:08:35 -0700

    make tests more concise

--- WDL/Error.py
+++ WDL/Error.py
@@ -3,36 +3,36 @@ from typing import Any, List, Optional, Dict, Callable, NamedTuple, TypeVar
 import WDL.Type as T
 from WDL.Expr import SourcePosition, TVBase, TVApply
 
-class Error(Exception):
+class Base(Exception):
     expr : TVBase
     def __init__(self, expr : TVBase, message : str) -> None:
         self.expr = expr
         message = "(Ln {}, Col {}) {}".format(expr.pos.line, expr.pos.column, message) # pyre-ignore
         super().__init__(message)
 
-class NoSuchFunction(Error):
+class NoSuchFunction(Base):
     def __init__(self, expr : TVBase, name : str) -> None:
         super().__init__(expr, "No such function: " + name)
 
-class WrongArity(Error):
+class WrongArity(Base):
     def __init__(self, expr : TVApply, expected : int) -> None:
         super().__init__(expr, "{} expects {} argument(s)".format(expr.name, expected)) # pyre-ignore
 
-class NotAnArray(Error):
+class NotAnArray(Base):
     def __init__(self, expr : TVBase) -> None:
         super().__init__(expr, "Not an array")
 
-class StaticTypeMismatch(Error):
+class StaticTypeMismatch(Base):
     def __init__(self, expr : TVBase, expected : T.Base, actual : T.Base, message : Optional[str] = None) -> None:
         msg = "Expected {} instead of {}".format(str(expected), str(actual))
         if message is not None:
             msg = msg + "; " + message
         super().__init__(expr, msg)
 
-class IncompatibleOperand(Error):
+class IncompatibleOperand(Base):
     def __init__(self, expr : TVBase, message : str) -> None:
         super().__init__(expr, message)
 
-class OutOfBounds(Error):
+class OutOfBounds(Base):
     def __init__(self, expr: TVBase) -> None:
         super().__init__(expr, "Array index out of bounds")
--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, inspect
 from typing import Optional
 from .context import WDL
 
@@ -26,77 +26,109 @@ class TestEval(unittest.TestCase):
         self.assertEqual(val, WDL.Value.Boolean(False))
         self.assertNotEqual(val, WDL.Value.Boolean(True))
 
-    def _t(self, expr : str, expected_value : str, env : WDL.Expr.Env = WDL.Expr.Env(),
-           expected_type : Optional[WDL.Type.Base] = None):
-        v = WDL.parse_expr(expr).eval(env).expect(expected_type)
-        self.assertEqual(str(v), expected_value)
+    def _test_tuples(self, *tuples):
+        for tuple in tuples:
+            assert(len(tuple) >= 2)
+            expr = tuple[0]
+            expected = tuple[1]
+            env = None
+            expected_type = None
+            exn = None
+            for x in tuple[2:]:
+                if isinstance(x, WDL.Expr.Env):
+                    env = x
+                elif isinstance(x, WDL.Type.Base):
+                    expected_type = x
+                elif inspect.isclass(x):
+                    exn = x
+                else:
+                    assert False
+            if exn:
+                with self.assertRaises(exn, msg=expected):
+                    WDL.parse_expr(expr).eval(env)
+            else:
+                v = WDL.parse_expr(expr).eval(env).expect(expected_type)
+                self.assertEqual(str(v), expected)
 
     def test_logic(self):
-        self._t("true && true", "true", WDL.Type.Boolean())
-        self._t("true && false", "false")
-        self._t("false && true", "false")
-        self._t("false && false", "false")
-        self._t("true || true", "true")
-        self._t("true || false", "true")
-        self._t("false || true", "true")
-        self._t("false || false", "false")
-        self._t("false && true || true && true", "true")
-        self._t("true && !false || false", "true")
-        self._t("!true", "false")
-        self._t("!!true", "true")
-        self._t("!false", "true")
-        self._t("!false && true", "true")
-        self._t("!(false && true)", "true")
+        self._test_tuples(
+            ("true && true", "true", WDL.Type.Boolean()),
+            ("true && false", "false"),
+            ("false && true", "false"),
+            ("false && false", "false"),
+            ("true || true", "true"),
+            ("true || false", "true"),
+            ("false || true", "true"),
+            ("false || false", "false"),
+            ("false && true || true && true", "true"),
+            ("true && !false || false", "true"),
+            ("!true", "false"),
+            ("!!true", "true"),
+            ("!false", "true"),
+            ("!false && true", "true"),
+            ("!(false && true)", "true")
+        )
 
     def test_arithmetic(self):
-        self._t("1", "1")
-        self._t("-1","-1")
-        self._t("0-1","-1")
-        self._t("1+1", "2")
-        self._t("2*3+4","10")
-        self._t("2*(3+4)","14")
-        self._t("2+3*4","14")
-        self._t("1+6/3*4","9")
-        self._t("1-4/3","0")
-        self._t("1--4/3","3") # -4/3 == -2
-        self._t("4%2","0")
-        self._t("4%3","1")
+        self._test_tuples(
+            ("1", "1"),
+            ("-1","-1"),
+            ("0-1","-1"),
+            ("1+1", "2"),
+            ("2*3+4","10"),
+            ("2*(3+4)","14"),
+            ("2+3*4","14"),
+            ("1+6/3*4","9"),
+            ("1-4/3","0"),
+            ("1--4/3","3"), # -4/3 == -2, is this right?
+            ("4%2","0"),
+            ("4%3","1"),
+            ("1 + false", "(Ln 1, Col 1) Non-numeric operand to + operator", WDL.Error.IncompatibleOperand)
+        )
 
     def test_cmp(self):
-        self._t("1 == 1","true")
-        self._t("1 == 0","false")
-        self._t("1 != 1","false")
-        self._t("1 != 0","true")
-        self._t("1 < 1","false")
-        self._t("1 <= 1","true")
-        self._t("1<2","true")
-        self._t("1<=2","true")
-        self._t("1>1","false")
-        self._t("1>=1","true")
-        self._t("1>2","false")
-        self._t("1>=0","true")
-
-        self._t("3<2 || 1>=0","true")
-        self._t("3<2&&1>=0","false")
-        self._t("3<2&&1>=0||1==1","true")
+        self._test_tuples(
+            ("1 == 1","true"),
+            ("1 == 0","false"),
+            ("1 != 1","false"),
+            ("1 != 0","true"),
+            ("1 < 1","false"),
+            ("1 <= 1","true"),
+            ("1<2","true"),
+            ("1<=2","true"),
+            ("1>1","false"),
+            ("1>=1","true"),
+            ("1>2","false"),
+            ("1>=0","true"),
+            ("3<2 || 1>=0","true"),
+            ("3<2&&1>=0","false"),
+            ("3<2&&1>=0||1==1","true"),
+            ("1 == false", "(Ln 1, Col 1) Cannot compare Int and Boolean", WDL.Error.IncompatibleOperand)
+        )
 
     def test_str(self):
-        self._t('"true"', '"true"', WDL.Type.String())
-        self._t('"true" == "true"', 'true', WDL.Type.Boolean())
-        self._t('"true" != "true"', 'false', WDL.Type.Boolean())
-        self._t('"true" == "foo"', 'false', WDL.Type.Boolean())
-        self._t('"true" != "bar"', 'true', WDL.Type.Boolean())
-        self._t('"foo" + "bar"', '"foobar"')
-        self._t('"foo" + 1', '"foo1"')
-        self._t('2.0 + "bar"', '"2.0bar"')
+        self._test_tuples(
+            ('"true"', '"true"', WDL.Type.String()),
+            ('"true" == "true"', 'true', WDL.Type.Boolean()),
+            ('"true" != "true"', 'false', WDL.Type.Boolean()),
+            ('"true" == "foo"', 'false', WDL.Type.Boolean()),
+            ('"true" != "bar"', 'true', WDL.Type.Boolean()),
+            ('"foo" + "bar"', '"foobar"'),
+            ('"foo" + 1', '"foo1"'),
+            ('2.0 + "bar"', '"2.0bar"')
+        )
 
     def test_if(self):
-        self._t("if false then 0 else 1","1", WDL.Type.Int())
-        self._t("if true then 0 else 1","0")
-        self._t("if false then 0 else 1+2","3")
-        self._t("(if false then 0 else 1)+2","3")
-        self._t("(if 1>0 then 1+1 else 1)+1","3")
-        self._t("if 1>0 then if true then 1 else 2 else 3","1")
+        self._test_tuples(
+            ("if false then 0 else 1","1", WDL.Type.Int()),
+            ("if true then 0 else 1","0"),
+            ("if false then 0 else 1+2","3"),
+            ("(if false then 0 else 1)+2","3"),
+            ("(if 1>0 then 1+1 else 1)+1","3"),
+            ("if 1>0 then if true then 1 else 2 else 3","1"),            
+            ("if 3.14 then 0 else 1", "(Ln 1, Col 1) Expected Boolean instead of Float; in if condition", WDL.Error.StaticTypeMismatch),
+            ("if 0 < 1 then 0 else false", "(Ln 1, Col 1) Expected Int instead of Boolean; if consequent & alternative must have the same type", WDL.Error.StaticTypeMismatch)
+        )
 
     def test_array(self):
         expr = WDL.parse_expr("[true,false]")
@@ -108,42 +140,32 @@ class TestEval(unittest.TestCase):
         self.assertEqual(str(val.type), "Array[Boolean]")
         self.assertEqual(str(val), "[true, false]")
 
-        self._t("[true, false][0]", "true")
-        self._t("[true, false][1]", "false")
-
-        self._t("[1+2, 3*4][1]", "12")
-        self._t("[]","[]", WDL.Type.AnyArray())
-        self._t("[] == []","true")
-        # TODO: comparison of arrays and other composite values
-        # self._t("[1] == []","false")
+        self._test_tuples(
+            ("[true, false][0]", "true"),
+            ("[true, false][1]", "false"),
+            ("[1+2, 3*4][1]", "12"),
+            ("[]","[]", WDL.Type.AnyArray()),
+            ("[] == []","true"),
+            ("[1, false]", "(Ln 1, Col 1) Expected Int instead of Boolean; inconsistent types within array", WDL.Error.StaticTypeMismatch),
+            ("1 + 2[3]", "(Ln 1, Col 5) Not an array", WDL.Error.NotAnArray),
+            ("[1, 2, 3][true]", "(Ln 1, Col 11) Expected Int instead of Boolean; Array index", WDL.Error.StaticTypeMismatch),
+            ("[1, 2, 3][4]", "(Ln 1, Col 11) Array index out of bounds", WDL.Error.OutOfBounds)
+        )
 
     def test_float_coercion(self):
-        self._t("1 + 1.0", "2.0", WDL.Type.Float())
-        self._t("1.0 + 1", "2.0", WDL.Type.Float())
-        self._t("1 == 1.0", "true")
-        self._t("1 == 1.1", "false")
-        self._t("1 != 1.1", "true")
-        self._t("1 < 1.0", "false")
-        self._t("1 <= 1.0", "true")
-        self._t("[1, 2.0]", "[1.0, 2.0]", WDL.Type.Array(WDL.Type.Float()))
-        self._t("[1, 2.0][0]", "1.0", WDL.Type.Float())
+        self._test_tuples(
+            ("1 + 1.0", "2.0", WDL.Type.Float()),
+            ("1.0 + 1", "2.0", WDL.Type.Float()),
+            ("1 == 1.0", "true"),
+            ("1 == 1.1", "false"),
+            ("1 != 1.1", "true"),
+            ("1 < 1.0", "false"),
+            ("1 <= 1.0", "true"),
+            ("[1, 2.0]", "[1.0, 2.0]", WDL.Type.Array(WDL.Type.Float())),
+            ("[1, 2.0][0]", "1.0", WDL.Type.Float())
+        )
 
     def test_errors(self):
-        with self.assertRaisesRegex(WDL.Error.NoSuchFunction, r"\(Ln 1, Col 5\) No such function: bogus") as cm:
-            self._t("1 + bogus(2)", None)
-        with self.assertRaisesRegex(WDL.Error.NotAnArray, r"\(Ln 1, Col 5\) Not an array") as cm:
-            self._t("1 + 2[3]", None)
-        with self.assertRaisesRegex(WDL.Error.StaticTypeMismatch, r"\(Ln 1, Col 1\) Expected Int instead of Boolean; inconsistent types within array") as cm:
-            self._t("[1, false]", None)
-        with self.assertRaisesRegex(WDL.Error.IncompatibleOperand, r"\(Ln 1, Col 1\) Non-numeric operand to \+ operator") as cm:
-            self._t("1 + false", None)
-        with self.assertRaisesRegex(WDL.Error.IncompatibleOperand, r"\(Ln 1, Col 1\) Cannot compare Int and Boolean") as cm:
-            self._t("1 == false", None)
-        with self.assertRaisesRegex(WDL.Error.StaticTypeMismatch, r"\(Ln 1, Col 1\) Expected Boolean instead of Float; in if condition") as cm:
-            self._t("if 3.14 then 0 else 1", None)
-        with self.assertRaisesRegex(WDL.Error.StaticTypeMismatch, r"\(Ln 1, Col 1\) Expected Int instead of Boolean; if consequent & alternative must have the same type") as cm:
-            self._t("if 0 < 1 then 0 else false", None)
-        with self.assertRaisesRegex(WDL.Error.StaticTypeMismatch, r"\(Ln 1, Col 11\) Expected Int instead of Boolean; Array index") as cm:
-            self._t("[1, 2, 3][true]", None)
-        with self.assertRaisesRegex(WDL.Error.OutOfBounds, r"\(Ln 1, Col 11\) Array index out of bounds") as cm:
-            self._t("[1, 2, 3][4]", None)
+        self._test_tuples(
+            ("1 + bogus(2)", "(Ln 1, Col 5) No such function: bogus", WDL.Error.NoSuchFunction)
+        )
