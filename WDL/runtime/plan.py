87f6e1bb2af16a92cb6bd6dc198f8a81c6afd8a9
Mike Lin <mlin@mlin.net>
2019-07-08 14:43:40 -0700

    wip

--- WDL/runtime/plan.py
+++ WDL/runtime/plan.py
@@ -14,31 +14,50 @@ Each node stores a list of dependencies, other nodes from which it has in-edges.
 from n1 to n2 represents the dependency of n2 on n1, usually including the flow of a
 ``WDL.Env.Values`` from n1 to n2.
 
-The plan is meant to be pickled easily, with each node assigned a readable ID.
+The plan is meant to be pickled easily, with each node assigned a readable ID, and dependencies
+referenced by such ID.
 """
 
 from abc import ABC, abstractmethod
-from typing import Tuple, List, Dict, Optional, Set, Iterable
+from typing import Tuple, List, Dict, Optional, Set, Iterable, Union
 from .. import Error, Env, Expr, Value, StdLib, Tree, _util
-from Error import SourceNode
+from ..Error import SourceNode
 from .error import *
 
 
 class Node(ABC):
     id: str
-    dependencies: Set[str]
+    _memo_dependencies: Optional[Set[str]]
 
     def __init__(self, id: str):
         self.id = id
-        self.dependencies = set()
+        self._memo_dependencies = None
+
+    @abstractmethod
+    @property
+    def source(self) -> Union[Tree.Decl, Tree.Call, Tree.Scatter, Tree.Conditional, Tree.Gather]:
+        ...
+
+    @property
+    def dependencies(self) -> Set[str]:
+        if self._memo_dependencies is None:
+            self._memo_dependencies = set(self._dependencies())
+        return self._memo_dependencies
+
+    def _dependencies(self) -> Iterable[str]:
+        return _expr_dependencies(getattr(self.source, "expr"))
 
 
 class Decl(Node):
-    source: Tree.Decl
+    _source: Tree.Decl
 
     def __init__(self, source: Tree.Decl) -> None:
         super().__init__(source.name)
-        self.source = source
+        self._source = source
+
+    @property
+    def source(self) -> Tree.Decl:
+        return self._source
 
 
 class WorkflowOutput(Decl):
@@ -48,91 +67,116 @@ class WorkflowOutput(Decl):
 
 
 class Call(Node):
-    source: Tree.Call
+    _source: Tree.Call
 
     def __init__(self, source: Tree.Call) -> None:
         super().__init__("call:" + source.name)
-        self.source = source
+        self._source = source
 
-    # TODO: consider brining back Binding and enumerating the individual call outputs...
+    @property
+    def source(self) -> Tree.Call:
+        return self._source
+
+    def _dependencies(self) -> Iterable[str]:
+        for expr in self.source.inputs.values():
+            yield from _expr_dependencies(expr)
 
 
 class Gather(Node):
-    source: Tree.Gather
+    _source: Tree.Gather
 
-    def __init__(self, source: Tree.Gather, referee_id: str) -> None:
-        super().__init__("gather:" + referee_id)
-        self.source = source
+    def __init__(self, source: Tree.Gather) -> None:
+        super().__init__("gather:" + _wrap(source.referee).id)
+        self._source = source
+
+    @property
+    def source(self) -> Tree.Gather:
+        return self._source
+
+    def _dependencies(self) -> Iterable[str]:
+        yield _wrap(self.source.referee).id
 
 
 class Scatter(Node):
-    source: Tree.Scatter
+    _source: Tree.Scatter
     body: List[Node]
     gathers: List[Gather]
 
     def __init__(self, source: Tree.Scatter) -> None:
-        super().__init__("scatter:" + source.variable)
-        self.source = source
+        super().__init__(source.name)
+        self._source = source
         self.body = []
         self.gathers = []
 
+    @property
+    def source(self) -> Tree.Scatter:
+        return self._source
+
 
 class Conditional(Node):
-    source: Tree.Conditional
+    _source: Tree.Conditional
     body: List[Node]
     gathers: List[Gather]
 
     def __init__(self, source: Tree.Conditional) -> None:
-        super().__init__("if:L{}C{}".format(source.pos.line, source.pos.column))
-        self.source = source
+        super().__init__(source.name)
+        self._source = source
         self.body = []
         self.gathers = []
 
+    @property
+    def source(self) -> Tree.Conditional:
+        return self._source
 
-def compile(workflow: Tree.Workflow, workflow_inputs: Env.Values) -> List[Node]:
-    #    - does typechecker currently detect circularities involving scatter expression? eg
-    #      scatter (i in arr) {
-    #          int j = i
-    #      }
-    #      Array[Int] i = j
-    ans = []
-    def visit(elt: )
-    for elt in (workflow.inputs or []):
-        
+
+def compile(workflow: Tree.Workflow, workflow_inputs: Env.Values) -> Iterable[Node]:
+    gathers: Dict[int, List[Tree.Gather]] = {}  # indexed by id(tree section object)
+
+    def visit(
+        elt: Union[Tree.Decl, Tree.Call, Tree.Scatter, Tree.Conditional, Tree.Gather]
+    ) -> Iterable[Node]:
+        yield _wrap(elt)
+        if isinstance(elt, (Tree.Scatter, Tree.Conditional)):
+            assert id(elt) not in gathers
+            gathers[id(elt)] = []
+            for ch in elt.elements:
+                yield from visit(ch)
+                if isinstance(ch, (Tree.Decl, Tree.Call)):
+                    g = Tree.Gather(section=elt, referee=ch)
+                    gathers[id(elt)].append(g)
+                    yield _wrap(g)
+                elif isinstance(ch, (Tree.Scatter, Tree.Conditional)):
+                    for subgather in gathers[id(ch)]:
+                        g = Tree.Gather(section=elt, referee=subgather)
+                        gathers[id(elt)].append(g)
+                        yield _wrap(g)
+                else:
+                    assert False
+
+    for elt in workflow.inputs or []:
+        yield from visit(elt)
     for elt in workflow.elements:
-    raise NotImplementedError()
+        yield from visit(elt)
+    for elt in workflow.outputs or []:
+        yield from visit(elt)
 
 
-def _new_node(elt: Union[Tree.Decl, Tree.Call, Tree.Gather, Tree.Scatter, Tree.Conditional], populate_dependencies: bool=True):
+def _wrap(elt: Union[Tree.Decl, Tree.Call, Tree.Scatter, Tree.Conditional, Tree.Gather]) -> Node:
     if isinstance(elt, Tree.Decl):
-        ans = Decl(elt)
-        ans.dependencies = _expr_dependencies(elt.expr) if populate_dependencies else set()
-        return ans
+        return Decl(elt)
     if isinstance(elt, Tree.Call):
-        ans = Call(elt)
-        for expr in call.inputs.values() if populate_dependencies else []:
-            ans.dependencies.update(_expr_dependencies(expr))
-        return ans
-    if isinstance(elt, Tree.Gather):
-        dep_id = _new_node(elt.referee, populate_dependencies=False).id
-        ans = Gather(elt, dep_id)
-        if populate_dependencies:
-            ans.dependencies.add(dep_id)
-        return ans
+        return Call(elt)
     if isinstance(elt, Tree.Scatter):
-        ans = Scatter(elt)
-        ans.dependencies = _expr_dependencies(elt.expr) if populate_dependencies else set()
-        return ans
+        return Scatter(elt)
     if isinstance(elt, Tree.Conditional):
-        ans = Conditional(elt)
-        ans.dependencies = _expr_dependencies(elt.expr) if populate_dependencies else set()
-        return ans
+        return Conditional(elt)
+    if isinstance(elt, Tree.Gather):
+        return Gather(elt)
     assert False
 
-def _expr_dependencies(expr: Expr.Base) -> Set[str]:
+
+def _expr_dependencies(expr: Expr.Base) -> Iterable[str]:
     if isinstance(expr, Expr.Ident):
-        return set([_new_node(expr.referee, populate_dependencies=False).id])
-    ans = set()
+        yield _wrap(expr.referee).id
     for ch in expr.children:
-        ans.update(_expr_dependencies(ch))
-    return ans
+        yield from _expr_dependencies(ch)
