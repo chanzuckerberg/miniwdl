chanzuckerberg/miniwdl

commit 6a33334db234d9ce1879098f924f7d044da520c5

miniwdl cromwell: disable --path since it doesn't work (issue #131)

WDL/CLI.py
@@ -34,7 +34,7 @@ def main(args=None):
     subparsers.required = True
     subparsers.dest = "command"
     fill_common(fill_check_subparser(subparsers))
-    fill_common(fill_cromwell_subparser(subparsers))
+    fill_common(fill_cromwell_subparser(subparsers), path=False)  # FIXME path issue #131
     fill_common(fill_run_subparser(subparsers))
 
     argcomplete.autocomplete(parser)
@@ -77,21 +77,22 @@ class PipVersionAction(Action):
         sys.exit(0)
 
 
-def fill_common(subparser):
+def fill_common(subparser, path=True):
     subparser.add_argument(
         "--no-quant-check",
         dest="check_quant",
         action="store_false",
         help="relax static typechecking of optional types, and permit coercion of T to Array[T] (discouraged; for backwards compatibility with older WDL)",
     )
-    subparser.add_argument(
-        "-p",
-        "--path",
-        metavar="DIR",
-        type=str,
-        action="append",
-        help="local directory to search for imports",
-    )
+    if path:
+        subparser.add_argument(
+            "-p",
+            "--path",
+            metavar="DIR",
+            type=str,
+            action="append",
+            help="local directory to search for imports",
+        )
     subparser.add_argument("--debug", action="store_true", help="show full exception traceback")
 
 
@@ -770,6 +771,7 @@ def cromwell(
     )
 
     for p in path:
+        # FIXME issue #131
         cromwell_cmd.append("--imports")
         cromwell_cmd.append(p)
     print(" ".join(["+"] + [shellquote(s) for s in cromwell_cmd]), file=sys.stderr)

tests/cromwell.t
@@ -11,7 +11,7 @@ source tests/bash-tap/bash-tap-bootstrap
 export PYTHONPATH="$SOURCE_DIR:$PYTHONPATH"
 miniwdl="python3 -m WDL"
 
-plan tests 59
+plan tests 61
 
 DN=$(mktemp -d --tmpdir miniwdl_cromwell_tests_XXXXXX)
 cd $DN
@@ -169,5 +169,29 @@ is "$(ls scatterrun/outputs/echo.t.out_f/0/2)" "fox" "scatter product 0 fox link
 is "$(ls scatterrun/outputs/echo.t.out_f/1/0)" "quick" "scatter product 1 quick link"
 is "$(ls scatterrun/outputs/echo.t.out_f/1/1)" "brown" "scatter product 1 brown link"
 is "$(ls scatterrun/outputs/echo.t.out_f/1/2)" "fox" "scatter product 1 fox link"
+
+mkdir my_imports
+cat << 'EOF' > my_imports/my_hello.wdl
+version 1.0
+task hello {
+    command {}
+    output {
+        String message = "Hello, world!"
+    }
+}
+EOF
+cat << 'EOF' > importer1.wdl
+version 1.0
+import "my_imports/my_hello.wdl"
+workflow wf {
+    call my_hello.hello
+    output {
+        String message = hello.message
+    }
+}
+EOF
+$miniwdl cromwell importer1.wdl | tee stdout
+is "$?" "0" "relative importer"
+is "$(jq -r '.outputs["wf.message"]' stdout)" "Hello, world!" "relative importer output"
 
 rm -rf $DN

commit 517cd85d37e6e865036133e2e8d35b7442ee63ad

fix cromwell arg completer which was broken by 6a33334

WDL/CLI.py
@@ -366,7 +366,12 @@ def runner_input_completer(prefix, parsed_args, **kwargs):
             argcomplete.warn("file not found: " + uri)
             return []
         try:
-            doc = load(uri, parsed_args.path, parsed_args.check_quant, read_source=read_source)
+            doc = load(
+                uri,
+                path=(parsed_args.path if hasattr(parsed_args, "path") else []),
+                check_quant=parsed_args.check_quant,
+                read_source=read_source,
+            )
         except Exception as exn:
             argcomplete.warn(
                 "unable to load {}; try 'miniwdl check' on it ({})".format(uri, str(exn))