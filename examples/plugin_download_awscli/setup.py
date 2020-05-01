4bb1f8b594192f5d72e86a3819fe70f0647a403f
Mike Lin <dna@mlin.net>
2020-05-01 00:36:40 -1000

    update examples/plugin_download_awscli

--- examples/plugin_download_awscli/setup.py
+++ examples/plugin_download_awscli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miniwdl_download_awscli',
-    version='0.0.1',
+    version='0.1.0',
     description='miniwdl download plugin for s3:// using awscli',
     author='Wid L. Hacker',
     py_modules=["miniwdl_download_awscli"],
