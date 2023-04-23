chanzuckerberg/miniwdl

commit eaa23bfef2a6d1bba4db1b9cf571e47a8f7a738f

version development: make input: keyword optional in calls
https://github.com/openwdl/wdl/pull/524

WDL/_grammar.py
@@ -299,7 +299,7 @@ conditional: "if" "(" expr ")" "{" inner_workflow_element* "}"
 call: "call" namespaced_ident ("after" CNAME)* _call_body? -> call
     | "call" namespaced_ident "as" CNAME ("after" CNAME)* _call_body? -> call_as
 namespaced_ident: CNAME ("." CNAME)*
-call_inputs: "input" ":" [call_input ("," call_input)*] ","?
+call_inputs: ["input" ":"] [call_input ("," call_input)*] ","?
 _call_body: "{" call_inputs? "}"
 call_input: CNAME ["=" expr]
 

test_corpi/contrived/issue596.wdl
@@ -25,7 +25,6 @@ workflow w {
         File? f
     }
     call t {
-        input:
         s_in = f,
         struct1 = Struct1 { strings:[f, f] }
     }

commit a6ef7cfa048ba80450b07820fb47249a7a5be45f

make call input: keyword required in WDL 1.1 again
eaa23bf inadvertently made it optional in 1.1 as well as development

WDL/_grammar.py
@@ -299,7 +299,8 @@ conditional: "if" "(" expr ")" "{" inner_workflow_element* "}"
 call: "call" namespaced_ident ("after" CNAME)* _call_body? -> call
     | "call" namespaced_ident "as" CNAME ("after" CNAME)* _call_body? -> call_as
 namespaced_ident: CNAME ("." CNAME)*
-call_inputs: ["input" ":"] [call_input ("," call_input)*] ","?
+call_inputs: input_colon? [call_input ("," call_input)*] ","?
+input_colon: "input" ":"
 _call_body: "{" call_inputs? "}"
 call_input: CNAME ["=" expr]
 

WDL/_parser.py
@@ -514,8 +514,23 @@ class _DocTransformer(_ExprTransformer):
             return (items[0].value, items[1])
         return (items[0].value, Expr.Ident(self._sp(meta), items[0].value))
 
+    def input_colon(self, meta, items):
+        return "input:"
+
     def call_inputs(self, meta, items):
         d = dict()
+        if items:
+            input_colon = False
+            if isinstance(items[0], str) and items[0] == "input:":
+                input_colon = True
+                items = items[1:]
+            if self._version == "1.1" and not input_colon:
+                raise Error.SyntaxError(
+                    self._sp(meta),
+                    "WDL 1.1 calls require input: keyword",
+                    self._version,
+                    self._declared_version,
+                )
         for k, v in items:
             if k in d:
                 raise Error.MultipleDefinitions(self._sp(meta), f"duplicate call input '{k}'")

tests/test_2calls.py
@@ -705,3 +705,18 @@ class TestCalls(unittest.TestCase):
         """
         doc = WDL.parse_document(txt)
         doc.typecheck()
+
+    def test_input_colon(self):
+        # the shared 1.1/development grammar makes input: keyword optional, but it's still required
+        # in WDL 1.1.
+        w = """
+        workflow w {
+            call sum {
+                x = 1, y = 1
+            }
+        }
+        """
+        doc = WDL.parse_document("version development\n" + tsk + w)
+        doc.typecheck()
+        with self.assertRaises(WDL.Error.SyntaxError):
+            doc = WDL.parse_document("version 1.1\n" + tsk + w)